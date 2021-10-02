172c43d0975b24b10ea2202c280ab270cba2779f
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-10-02 12:09:33 +0000

    [Add] 管理者ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+  devise_for :admin, controllers: {
+  sessions:      'admin/sessions',
+  passwords:     'admin/passwords',
+  registrations: 'admin/registrations'
+}
+devise_for :customers, controllers: {
+  sessions:      'customers/sessions',
+  passwords:     'customers/passwords',
+  registrations: 'customers/registrations'
+}
+
+root to: 'public/homes#top'
+get "/about" => "public/homes#about"
+
+get "/items" => "public/items#index"
+get "/items/:id" => "public/items#show"
+
+namespace :admin do
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
