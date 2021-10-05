e0e52f79a5545d55e667d7d5c6652ceabd1b0d24
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-10-05 06:16:38 +0000

    [Add] 管理者側/会員一覧画面・会員詳細画面・会員情報編集画面

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,10 @@ namespace :admin do
 namespace :admin do
     resources :items, only:[:index, :new, :create, :show, :edit, :update]
   end
+  
+namespace :admin do
+  resources :customers, only:[:index, :show, :edit, :update]
+end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
