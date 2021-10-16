59eeef4463f4d02d0bd26ba46440931aa51df455
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-10-16 09:53:51 +0000

    [Add] 配送先登録・一覧画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-def after_sign_out_path_for(resource_or_scope)
-    if resource_or_scope == :customer
-        root_path
-    elsif resource_or_scope == :admin
-        new_admin_session_path
-    else
-        root_path
-    end
-end
+  def after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :customer
+          root_path
+      elsif resource_or_scope == :admin
+          new_admin_session_path
+      else
+          root_path
+      end
+  end
 
-private
+  protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :telephone_number])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :telephone_number, :postal_code])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ scope module: :public do
   # patch "/customers" => "customers#update"
   get "/customers/unsubscribe" => "customers#unsubscribe"
   patch "/customers/withdraw" => "customers#withdraw"
+  resources :addresses, only:[:index, :edit, :create, :update, :destroy]
 end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
