cf5f11d45ed61c86b25d543adb427680a248cf9a
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-10-17 07:51:05 +0000

    [Add] 会員側商品一覧画面

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,12 @@
 class Public::ItemsController < ApplicationController
-  
+
   def index
+    @items = Item.all
+    @genres = Genre.all
   end
-  
+
   def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 end
