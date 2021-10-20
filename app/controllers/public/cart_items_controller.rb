f62f8ceaa9106ea14d68fbe9e689724912ace2eb
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-10-20 14:41:14 +0000

    [Add] カート画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,18 +1,40 @@
 class Public::CartItemsController < ApplicationController
 
   def index
+    @customer = current_customer
+    @cart_items = @customer.cart_items
+    @total = 0
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
+    @customer = current_customer
+    @customer.cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 
 end
