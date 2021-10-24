b7a49dc49aa0fde49df5564d42e46872fd1881d1
EC2 Default User <ec2-user@ip-172-31-74-17.ec2.internal>
2021-10-24 07:58:52 +0000

    [Add] 注文情報入力画面

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :customer
-  
+
+  def full_address
+    '〒' + postal_code + ' ' + address + ' ' + name
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :addresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
-  
+
+  def full_address
+    '〒' + postal_code + ' ' + address + ' ' + last_name + first_name
+  end
+
 end
