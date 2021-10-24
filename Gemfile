a36de2e577c3061afd09f804705a8a50294a4967
EC2 Default User <ec2-user@ip-172-31-74-17.ec2.internal>
2021-10-24 05:04:34 +0000

    [Add] order.rbにenum導入

--- Gemfile
+++ Gemfile
@@ -67,3 +67,4 @@ gem 'bootstrap', '~> 4.5'
 gem 'jquery-rails'
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
+gem "enum_help"
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
-  
-  enum payment_method:{
-    credit: 0,
-    bank: 1,
-  }
-  
-  enum status:{
-    waiting: 0,
-    paid_up: 1,
-    making: 2,
-    preparing: 3,
-    shipped: 4,
-  }
-  
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
+
+  enum status: { waiting: 0, paid_up: 1, making: 2, preparing: 3, shipped: 4 }
+
 end
