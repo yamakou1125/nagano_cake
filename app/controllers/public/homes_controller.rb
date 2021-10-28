d08554a810127c7bfe8f53ff53cf4c9e402673d9
EC2 Default User <ec2-user@ip-172-31-74-17.ec2.internal>
2021-10-28 09:44:53 +0000

    [Add] ジャンル検索機能

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,11 +1,12 @@
 class Public::HomesController < ApplicationController
-  
+
   def top
+    @genres = Genre.all
     @items = Item.last(4)
   end
-  
+
   def about
   end
-  
-  
+
+
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,10 +5,21 @@ class Public::ItemsController < ApplicationController
     @genres = Genre.all
   end
 
+  def surch
+    @genres = Genre.all
+    @genre = Genre.find(params[:id])
+  end
+
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
     @genres = Genre.all
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id)
   end
 
 end
