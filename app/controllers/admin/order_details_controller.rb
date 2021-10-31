b43926d0ec0832f82f6f4aa03cdabf6e20fbdc1d
EC2 Default User <ec2-user@ip-172-31-74-17.ec2.internal>
2021-10-31 04:45:07 +0000

    [Update] 管理者側注文履歴詳細のステータス自動更新

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,12 +3,20 @@ class Admin::OrderDetailsController < ApplicationController
 before_action :authenticate_admin!
 
   def update
-   @order_detail = OrderDetail.find(params[:id])
+    @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
-    redirect_to admin_order_path(@order_detail.order.id)
+    @order = @order_detail.order
+    if @order_detail.making_status == "making"
+       @order.update(status: 2)
+    elsif @order.order_details.pluck(:making_status).all? {|making_status| making_status == "complete"}
+       @order = @order_detail.order
+       @order.update(status: 3)
+    end
+     redirect_to admin_order_path(@order_detail.order.id)
   end
 
-  private
+private
+
   def order_detail_params
     params.require(:order_detail).permit(:order_id, :item_id, :making_status)
   end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,6 +10,12 @@ before_action :authenticate_admin!
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    if Order.statuses[@order.status] == 1
+      @order_details = @order.order_details
+      @order_details.each do |order_detail|
+        order_detail.update(making_status: 1)
+      end
+    end
     redirect_to admin_order_path(@order.id)
   end
 
